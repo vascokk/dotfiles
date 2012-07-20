421d7e65578ff8e95a41b71fe89f4d56eece7d88
vas <vasco.kk@gmail.com>
2012-07-20 18:41:19 +0100

    Initial import

---
+++ .vimrc
@@ -0,0 +1,11 @@
+call pathogen#infect()
+syntax on
+filetype plugin indent on
+set mouse=a
+set ts=4
+colorscheme elflord
+set paste
+
+set nocompatible   " Disable vi-compatibility
+set laststatus=2   " Always show the statusline
+set encoding=utf-8 " Necessary to show unicode glyphs
---
+++ symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.bash_login ~/.bash_login
+ln -s ~/dotfiles/.bash_logout ~/.bash_logout
+ln -s ~/dotfiles/.bash_profile ~/bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.viminfo ~/.viminfo
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
